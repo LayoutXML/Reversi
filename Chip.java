e94899ee43aa39b4ea0d657940291929633f6048
LayoutXML <hello@layoutxml.com>
2018-09-20 16:29:34 +0100

    Planning

---
+++ Chip.java
@@ -0,0 +1,12 @@
+public class Chip {
+
+	private Boolean side; //white - true; black - false;
+	
+	public void changeSide(Boolean side) {
+		this.side = side;
+	}
+
+	public Boolean getSide() {
+		return side;
+	}
+}
---
+++ Game.java
@@ -0,0 +1,9 @@
+public class Game {
+	public Integer playerTurn;
+	private Board board = new Board;
+
+	public void main() {
+		board.createBoard();
+	}
+
+}
